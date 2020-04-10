c20777a6bfa31fc714ebb433cf81aeb44f77cd94
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2020-04-10 16:41:21 +0200

    ♻ Improve Icon component with new feature such as size or spin

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Icon = ({ icon, className }) => {
+const Icon = ({ icon, className, spin, size }) => {
   return (
-    <FontAwesomeIcon icon={icon} className={className} />
+    <FontAwesomeIcon
+      icon={icon}
+      className={className}
+      spin={spin}
+      size={size}
+    />
   );
 };
 
