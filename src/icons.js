bf962225925b30d974c71bf446375fd517b0093b
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2020-04-10 12:25:43 +0200

    ➕ Add fontawesome dependency

---
+++ src/icons.js
@@ -0,0 +1,4 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faDownload, faEye, faHeart } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faDownload, faEye, faHeart);
