f6d99adf22aa170f387a97e4dad67ff81bc2a248
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2020-04-10 18:17:18 +0200

    😁 Fix typo

--- src/components/NoImages.js
+++ src/components/NoImages.js
@@ -6,7 +6,7 @@ const NoImages = () => {
     <div className="w-full flex justify-center items-center mt-10">
       <h3>
         <Icon icon="frown" size="3x" className="text-indigo-400 mr-2" />
-        <span className="font-bold text-4xl">No images fund</span>
+        <span className="font-bold text-4xl">No images found</span>
       </h3>
     </div>
   );
